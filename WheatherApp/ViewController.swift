939fe8958ca30d9da923e0627aae6ad54f4fb4ad
Enrico Sousa Gollner <enricogollner.tech@gmail.com>
2024-02-27 00:04:18 -0300

    Starting project

---
+++ WheatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WheatherApp
+//
+//  Created by Enrico Sousa Gollner on 24/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
